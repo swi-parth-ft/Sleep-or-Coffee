206c8eee2643c3af4fcba8edc282722cbe84f7fa
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2023-01-17 12:49:48 -0500

    Initial Commit

---
+++ Sleep or Coffee/Sleep_or_CoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleep_or_CoffeeApp.swift
+//  Sleep or Coffee
+//
+//  Created by Parth Antala on 2023-01-17.
+//
+
+import SwiftUI
+
+@main
+struct Sleep_or_CoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
